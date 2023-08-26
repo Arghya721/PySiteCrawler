648652e4703e607f5a48894d9a8a4a83559e26ab
Arghya721 <imarghyadeep@gmail.com>
2023-08-26 14:55:14 +0530

    Changed Version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(str(ROOT_DIR / "Readme.md")) as readme:
 
 setup(
     name='PySiteCrawler',
-    version='0.1.1',
+    version='0.1.2',
     description='A web crawler that uses graph traversal algorithms to crawl the web.',
     long_description=long_description,
     long_description_content_type="text/markdown",
